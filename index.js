567866b7da4d76d65f974e259399884907c11d7c
Shreyansh <shreyanshtiwari2002excel@gmail.com>
2023-10-04 18:29:53 +0530

    dynamic_routing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const users = require('./MOCK_DATA.json')
 const PORT = 3000
 
 // GET request API
-app.get("/users/api", ( req, res )=>{
+app.get("/api/users", ( req, res )=>{
     return res.json(users);
 })
 
@@ -18,9 +18,31 @@ app.get("/users", ( req, res )=>{
         }).join("")}
     </ul>
     `
-
     return res.send(html);
 })
+
+// dynamic route for requests
+app
+.route("/api/users/:id")
+.get((req, res)=>{
+    const id = Number( req.params.id );
+    console.log(id);
+    return res.json(users[id-1]);
+})
+.post((req, res)=>{
+    const id = Number( req.params.id );
+    return res.json({ status : "pending"});
+})
+.patch((req, res)=>{
+    const id = Number( req.params.id );
+    return res.json({ status : "pending"});
+})
+.delete((req, res)=>{
+    const id = Number( req.params.id );
+    return res.json({ status : "pending"});
+})
+
+
 
 app.listen(PORT, ()=>{
     console.log("listening to port 3000"); 
